6e7e78e060f82b622bb781567769dc7d421d7f34
zBlackPvP <68575177+zBlackPvP@users.noreply.github.com>
2025-05-02 18:09:41 -0300

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // Adjust paths to match your project structure
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
